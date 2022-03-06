1383efe1f6ccdbc1e9a15106e52eaea76bdc6104
a21andreagt@iessanclemente.net <>
2022-03-06 17:30:15 +0100

    Renomea a clase Circulo por Aro, modifica clase de probas e os comentarios. Cambia todas as ocorrencias de Circulo por Aro.

--- src/main/java/circulo/Main.java
+++ src/main/java/circulo/Main.java
@@ -3,23 +3,23 @@ package circulo;
 import java.text.DecimalFormat;
 
 /**
- * Pruebas de refactorización en NetBeans con la clase Circulo
+ * Pruebas de refactorización en NetBeans con la clase Aro
  * @author Andrea Gutierrez Torres
  */
 public class Main {
     public static void main(String[] args) {
-        Circulo circulo = new Circulo(37,43,2.5);
+        Aro aro = new Aro(37,43,2.5);
         String saida =
-                "A coordenada X é "+circulo.obterX()+
-                "\nA coordenada Y é "+circulo.obterY()+
-                "\nO radio é "+circulo.obterRadio();
-        circulo.establecerX(35);
-        circulo.establecerY(20);
-        circulo.establecerRadio(4.2);
+                "A coordenada X é "+aro.obterX()+
+                "\nA coordenada Y é "+aro.obterY()+
+                "\nO radio é "+aro.obterRadio();
+        aro.establecerX(35);
+        aro.establecerY(20);
+        aro.establecerRadio(4.2);
         DecimalFormat dousDixitos = new DecimalFormat("0.00");
-        saida+="\nO diámetro é "+dousDixitos.format(circulo.obterDiametro());
-        saida+="\nA circunferencia é "+dousDixitos.format(circulo.obterCircunferencia());
-        saida+="\nA área é "+dousDixitos.format(circulo.obterArea());
+        saida+="\nO diámetro é "+dousDixitos.format(aro.obterDiametro());
+        saida+="\nA circunferencia é "+dousDixitos.format(aro.obterCircunferencia());
+        saida+="\nA área é "+dousDixitos.format(aro.obterArea());
         System.out.println(saida);
         
         System.out.println(saida);
